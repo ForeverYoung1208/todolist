5c0e478830b98899ea630a69dd4b65a1c455d7fe
ForeverYoung1208 <siafin2010@gmail.com>
2019-03-17 09:49:19 +0200

    a

--- app/javascript/packs/TodoApp/Project/Task/task.jsx
+++ app/javascript/packs/TodoApp/Project/Task/task.jsx
@@ -5,22 +5,15 @@ import {AppContext} from '../../todoApp'
 export default class Task extends React.Component {
 	constructor(props){
 		super(props)
+		this.state = {
+			hovered: false,
+		}
 	}
 
 	render = () => {
 		const {id, name, priority, deadline, status_id} = this.props.task
 		const {toggleTaskStatus} = this.context
-		return(
-			<AppContext.Consumer>
-				{(fns)=>
-					<tr>
-						<td>
-								<input type="checkbox" className="checkbox-task" checked={status_id == 4} onChange={()=> fns.toggleTaskStatus(id)}/>
-						</td>
-						<td className="td-empty"></td>
-						<td>{name} (id:{id}), till: {deadline}</td>
-						<td>
-							<div className="task-controls">
+		const taskControls = <div className="task-controls">
 								<div className="arrows">
 									<div className="fa fa-caret-up hoverable"></div>
 									<div className="horizontal-line"></div>
@@ -30,11 +23,23 @@ export default class Task extends React.Component {
 								<div className="fa fa-pen m-2 hoverable"></div>
 								<div className="vertical-line m-2"></div>
 								<div className="far fa-trash-alt m-2 hoverable"></div>
+		</div>
+		const noTaskControls = <div className="no-task-controls"></div>
 
-								
-							</div>
 
+		return(
+			<AppContext.Consumer>
+				{(fns)=>
+					<tr
+						onMouseEnter={e=> this.setState({hovered:true})}
+						onMouseLeave={e=> this.setState({hovered:false})}
+					>
+						<td>
+								<input type="checkbox" className="checkbox-task" checked={status_id == 4} onChange={()=> fns.toggleTaskStatus(id)}/>
 						</td>
+						<td className="td-empty"></td>
+						<td>{name} (id:{id}), till: {deadline}</td>
+						<td className="td-task-controls">{this.state.hovered ? taskControls : noTaskControls }</td>
 					</tr>
 				}
 			</AppContext.Consumer>
--- app/javascript/packs/TodoApp/Project/projectForm.jsx
+++ app/javascript/packs/TodoApp/Project/projectForm.jsx
@@ -4,19 +4,31 @@ import PropTypes from 'prop-types'
 export default class ProjectForm extends React.Component {
 	constructor(props){
 		super(props)
+		this.state ={
+			hovered: false
+		}
 	}
+
+
 
 	render = () => {
 		const {project} = this.props
+
+		const btnsEditProject = <div className="btns-edit-project ml-auto">
+			<div className='fa fa-pen p-2'></div> 
+			|
+			<div className='far fa-trash-alt p-2'></div> 
+		</div>
+
+
 		return(
-			<div className="project-head flex-row">
+			<div className="project-head flex-row" 
+				onMouseEnter={e=> this.setState({hovered:true})}
+				onMouseLeave={e=> this.setState({hovered:false})}
+			>
 				<div className='fa fa-calendar-alt shadow-blue p-2'></div> 
 				<div> {project.name} </div>
-				<div className="btns-edit-project ml-auto">
-					<div className='fa fa-pen p-2'></div> 
-					|
-					<div className='far fa-trash-alt p-2'></div> 
-				</div>
+					{this.state.hovered ? btnsEditProject : ''}
 			</div>
 		)
 	}
